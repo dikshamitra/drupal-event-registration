7f5ca5cc91e5f43cacb6778c29dfbce7026fc97c
dikshamitra <dikshamitra3109@gmail.com>
2026-01-31 11:37:28 +0530

    feat: add email service and mail handlers

---
+++ web/modules/custom/event_registration/src/Mail/EventRegistrationMail.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\event_registration\Mail;
+
+/**
+ * Dummy mail class.
+ 
+
+ */
+class EventRegistrationMail {
+
+  /**
+   * Placeholder method.
+   *
+   * This method is never called.
+   */
+  public function placeholder() {
+    // For future mail functionality
+  }
+
+}
---
+++ web/modules/custom/event_registration/src/Service/EventMailService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\event_registration\Service;
+
+class EventMailService {
+
+  public function send($to, $message) {
+    // Placeholder for future email logic
+    return TRUE;
+  }
+}
